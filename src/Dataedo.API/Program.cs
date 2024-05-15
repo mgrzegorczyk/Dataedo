4c2a89a36e399eb38fabe167a2a8074b7c332d4f
mgrzegor <m-grzegorczyk@o2.pl>
2024-05-15 18:18:54 +0200

    API Nlog implementation

--- src/Dataedo.API/Program.cs
+++ src/Dataedo.API/Program.cs
@@ -1,26 +1,43 @@
 using Dataedo.Application;
 using Dataedo.Infrastructure;
+using NLog.Web;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
-builder.Services.AddInfrastructure();
-builder.Services.AddApplication();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+var logger = NLogBuilder.ConfigureNLog("nlog.config")
+    .GetCurrentClassLogger();
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddControllers();
+    builder.Services.AddInfrastructure();
+    builder.Services.AddApplication();
+
+    builder.Host.UseNLog();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    logger.Error(ex, "Application stopped!");
+    throw;
+}
+finally
+{
+    NLog.LogManager.Shutdown();
+}
